a8dfdac446c74d69c395413191d6a13b92d8566b
Sai Amarnath G <112251208+Saiamarnath@users.noreply.github.com>
2025-01-12 10:51:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use("/api/v1/search",protectRoute,searchRoutes);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 
